4e53022d61bf87d9eae4b42cb99cc7c5a9274c7d
Hector <hector.longarte@gmail.com>
2021-12-15 11:28:01 +1100

    Deployment

--- README.md
+++
@@ -1,9 +0,0 @@
-# robofriends
-
-Simple React App that fetches images from https://robohash.org and details from https://jsonplaceholder.typicode.com
-
-## To run the project:
-
-1. Clone this repo
-2. Run `npm install`
-3. Run `npm start`
---
+++ precache-manifest.b3fee82292502297f96acd4632a31bc8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0ec79552b7b9a4aeda9d12f90cf811",
+    "url": "/robofriends-hooks/index.html"
+  },
+  {
+    "revision": "d53dc64789fe8e25ee07",
+    "url": "/robofriends-hooks/static/css/2.3b2cfc2e.chunk.css"
+  },
+  {
+    "revision": "ede0fe2cc94cb857a4e4",
+    "url": "/robofriends-hooks/static/css/main.e8270b9b.chunk.css"
+  },
+  {
+    "revision": "d53dc64789fe8e25ee07",
+    "url": "/robofriends-hooks/static/js/2.9ab57a0b.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/robofriends-hooks/static/js/2.9ab57a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ede0fe2cc94cb857a4e4",
+    "url": "/robofriends-hooks/static/js/main.78424dc1.chunk.js"
+  },
+  {
+    "revision": "4383c2c98e76d67b8fa9",
+    "url": "/robofriends-hooks/static/js/runtime-main.1b4d344e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-hooks/static/media/SEGA.12a3db8b.woff"
+  }
+]);
